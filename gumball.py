7e4bb8b03f136eeb44e35c6319890434b1883bbb
Lachlan Heywood <lheywood@hugeinc.com>
2016-04-03 22:03:16 -0400

    Switch to counter-based OTP authentication

--- gumball.py
+++ gumball.py
@@ -6,10 +6,11 @@ import time
 
 search_term = '#ignorethistweet'
 
-totp = pyotp.TOTP('hugeprizemachine')
+hotp = pyotp.HOTP('hugeprizemachine')
 lcd = AdafruitLcd()
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 current_user = ""
+current_counter = 1
 max_errors = 3
 
 def CheckFollow(screenname):
@@ -23,7 +24,7 @@ def CheckFollow(screenname):
 def CheckPassWithUser(user_screen_name):
 	message_text = "Thanks for sharing! Enter the current OTP on the screen to claim your prize!"
 	twitter.send_direct_message(screen_name=user_screen_name, text=message_text)
-	print "DM sent to user ", user_screen_name
+	print "DM sent to user @" + user_screen_name + '. Waiting for response...'
 	stream = UserStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 	global current_user
 	current_user = user_screen_name
@@ -44,17 +45,15 @@ def ClearAndPrint(message):
 
 class UserStreamer(TwythonStreamer):
 	def on_success(self, data):
+		global max_errors
+		global current_counter
+		message = "Thanks for tweeting!\2Please reply with\3these numbers:\4       " + hotp.at(current_counter)
 		ScreenOn()
-		message = "Thanks for tweeting!\2Get ready to enter\3the next numbers\4in 10 seconds"
-		ClearAndPrint(message)
-		time.sleep(11)
-		message = "Please reply with\2these numbers:\4" + totp.now()
 		ClearAndPrint(message)
 		if 'direct_message' in data:
 			if data['direct_message']['sender']['screen_name'] == current_user:
-				global max_errors
 				if max_errors:
-					validate = totp.verify(int(data['direct_message']['text']))
+					validate = hotp.verify(int(data['direct_message']['text']), current_counter)
 					if validate == True:
 						message = "Correct Code!\2Dispensing Now..."
 						ClearAndPrint(message)
@@ -65,10 +64,13 @@ class UserStreamer(TwythonStreamer):
 					else:
 						max_errors -= 1
 						print 'Authentication Failed. Try again...'
+					current_counter += 1
 				else:
 					print "Authentication Failed 3 times. Exiting."
 					self.disconnect()
 					ScreenOff()
+					max_errors = 3
+		print "Completed engagement with @" + current_user + ". Listening for next user."
 
 	def on_error(self, status_code, data):
 		print status_code, data
@@ -89,5 +91,5 @@ class SearchStreamer(TwythonStreamer):
 
 stream = SearchStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-print 'Listening for tweets containing the term:"' + search_term + '"'
+print 'Listening for tweets containing the term "' + search_term + '"'
 stream.statuses.filter(track=search_term)
